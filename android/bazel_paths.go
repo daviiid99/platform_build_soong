c17b795d2c0a8940011a346407b2412941e8fa4d
David <davidmolinamorales1999@gmail.com>
2021-09-01 15:10:22 +0200

    removed undefined: EarlyModulePathContext

--- android/bazel_paths.go
+++ android/bazel_paths.go
@@ -3,7 +3,7 @@
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+//https://github.com/daviiid99/platform_build_soong/raw/master/android/bazel_paths.go
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
@@ -70,7 +70,6 @@ import (
 // A subset of the ModuleContext methods which are sufficient to resolve references to paths/deps in
 // order to form a Bazel-compatible label for conversion.
 type BazelConversionPathContext interface {
-	EarlyModulePathContext
 
 	GetDirectDep(name string) (blueprint.Module, blueprint.DependencyTag)
 	ModuleFromName(name string) (blueprint.Module, bool)
